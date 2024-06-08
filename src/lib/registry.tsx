d06b3c26f811026b50c7bf2b8c478ef5ed331be3
Hamidreza Hanafi <hamidreza.hanafi@faire.com>
2024-06-08 13:21:34 -0400

    Fix streaming hydration

--- src/lib/registry.tsx
+++ src/lib/registry.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { useServerInsertedHTML } from "next/navigation";
-import { ServerStyleSheet, StyleSheetManager } from "styled-components";
+import { ServerStyleSheet, StyleSheetManager, StyleSheetContext } from "styled-components";
 
 export default function StyledComponentsRegistry({
   children,
@@ -13,13 +13,27 @@ export default function StyledComponentsRegistry({
   // x-ref: https://reactjs.org/docs/hooks-reference.html#lazy-initial-state
   const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet());
 
+  const { styleSheet: clientStyleSheet } = useContext(StyleSheetContext);
+  
   useServerInsertedHTML(() => {
     const styles = styledComponentsStyleSheet.getStyleElement();
     styledComponentsStyleSheet.instance.clearTag();
-    return <>{styles}</>;
+    return (
+      <>
+        {styles}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.SC_SS?.rehydrate?.()`
+          }}
+        ></script>
+      </>
+    );
   });
 
-  if (typeof window !== "undefined") return <>{children}</>;
+  if (typeof window !== "undefined") {
+    window.SC_SS = clientStyleSheet;
+    return <>{children}</>;
+  }
 
   return (
     <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>
